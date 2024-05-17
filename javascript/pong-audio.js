7b64ac668eec7ed10cc4e41016117ff54d48b051
Alyssa Huddleston <alyssa459@att.net>
2024-05-16 19:44:35 -0500

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -52,7 +52,7 @@ soundArray.push(paddleSound);
 export var scoreSound = new soundFile("achievement.mp3");
 soundArray.push(scoreSound);
 
-export var ambientSound = new soundFile("night.mp3");
+export var ambientSound = new soundFile("blue.mp3");
 soundArray.push(ambientSound);
 ambientSound.player.loop = true; //turn on looping
 ambientSound.player.volume.value = -20; //turn down volume
